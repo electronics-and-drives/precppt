9d4166751a78dfe0e2cc0a4748126769ed8666fb
Yannick Uhlmann <augustunderground@protonmail.com>
2021-06-08 18:14:10 +0200

    config reading in separate function  for later use

--- pcp.cpp
+++ pcp.cpp
@@ -11,7 +11,23 @@ PreceptModule::PreceptModule(const char* modelPath, const char* configPath)
     catch (const c10::Error& e) 
         { std::cerr << "error loading the model\n"; }
     
-    YAML::Node config = YAML::LoadFile(configPath);
+    readYAMLcfg(configPath);
+}
+
+// Read YAML Configuration generated from PRECEPT Training.
+//  Takes a path to a *.yml config file and reads the nodes
+//  into class attributes.
+//  readYAMLcfg :: [char] -> bool
+bool PreceptModule::readYAMLcfg(const char* configPath)
+{
+    YAML::Node config;
+    try 
+        { config = YAML::LoadFile(configPath); }
+    catch (...) 
+    { 
+        std::cerr << "Error Loading " << configPath << std::endl; 
+        return false;
+    }
 
     numX = config["num_x"].as<int>();
     numY = config["num_y"].as<int>();
@@ -29,6 +45,8 @@ PreceptModule::PreceptModule(const char* modelPath, const char* configPath)
     
     paramsX = config["params_x"].as<std::vector<std::string>>();
     paramsY = config["params_y"].as<std::vector<std::string>>();
+
+    return true;
 }
 
 // Predict :: [float] -> [float]
@@ -49,3 +67,13 @@ float* PreceptModule::predict(const float* x)
 // Getters
 int PreceptModule::getNumInputs() const {return numX;}
 int PreceptModule::getNumOutputs() const {return numY;}
+std::vector<float> PreceptModule::getMaxX() const {return maxX;}
+std::vector<float> PreceptModule::getMinX() const {return minX;}
+std::vector<float> PreceptModule::getMaxY() const {return maxY;}
+std::vector<float> PreceptModule::getMinY() const {return minY;}
+std::vector<float> PreceptModule::getLambdaX() const {return lambdaX;}
+std::vector<float> PreceptModule::getLambdaY() const {return lambdaY;}
+std::vector<std::string> PreceptModule::getMaskX() const {return maskX;}
+std::vector<std::string> PreceptModule::getMaskY() const {return maskY;}
+std::vector<std::string> PreceptModule::getParamsX() const {return paramsX;}
+std::vector<std::string> PreceptModule::getParamsY() const {return paramsY;}
